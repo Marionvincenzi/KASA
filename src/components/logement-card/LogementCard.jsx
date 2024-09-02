22b9644788912a7636d8e3bf719a0a06ee8046a9
Marionvincenzi <marion.vincenzi7@orange.fr>
2024-09-02 15:31:21 +0200

    list

--- src/components/logement-card/LogementCard.jsx
+++ src/components/logement-card/LogementCard.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types"; // Si vous souhaitez ajouter des PropTypes pour validation
 import "./LogementCard.css";
+import logements from "../../datas/logement.json";
 
-const LogementCard = ({ logement }) => {
+const LogementCard = ({ logements }) => {
     
     return (
         <article className="card">
-            <Link to={`/logement/${logement.id}`} className="card-link">
+            <Link to={`/logement/${logements.id}`} className="card-link">
                 <div className="img-container">
-                    <img src={logement.cover} alt={logement.title} />
+                    <img src={logements.cover} alt={logements.title} />
                 </div>
                 <div className="card-title">
-                    <h2>{logement.title}</h2>
+                    <h2>{logements.title}</h2>
                 </div>
             </Link>
         </article>
@@ -19,12 +19,5 @@ const LogementCard = ({ logement }) => {
 };
 
   
-LogementCard.propTypes = {
-    logement: PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        cover: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-    }).isRequired,
-};
 
 export default LogementCard;
