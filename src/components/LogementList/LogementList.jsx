068da043d739316cef6b0e31161a7c7658d1fa76
Marionvincenzi <marion.vincenzi7@orange.fr>
2024-09-02 21:17:46 +0200

    cartes

--- src/components/LogementList/LogementList.jsx
+++ src/components/LogementList/LogementList.jsx
@@ -1,26 +1,18 @@
-import info from "../../datas/logement.json";
-import { Link } from "react-router-dom";
+import logements from "../../datas/logement.json";
+import LogementCard from "../logement-card/LogementCard";
 
 const LogementsList = () => {
-    return (
-        <div className="logements-list">
-            <main>
-                <section id="logements" className="logements">
-                    <article className="logements-cards">
-                        {info.map((logement) => (
-                            <div className="card" key={logement.id}>
-                                <Link to={`/fiche/${logement.id}`}>
-                                <div className="logements-container">
-                                    <h2>{logement.title}</h2>
-                                </div>
-                                </Link>
-                            </div>
-                        ))}
-                    </article>
-                </section>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div className="logements-list">
+      <main>
+        <section id="logements" className="logements">
+          {logements.map((logement) => (
+            <LogementCard key={logement.id} logement={logement} />
+          ))}
+        </section>
+      </main>
+    </div>
+  );
+};
 
 export default LogementsList;
--- src/components/logement-card/LogementCard.jsx
+++ src/components/logement-card/LogementCard.jsx
@@ -1,21 +1,18 @@
+
 import { Link } from "react-router-dom";
 import "./LogementCard.css";
-import logements from "../../datas/logement.json";
-
-const LogementCard = ({ logements }) => {
-    
-    return (
-        <article className="card">
-            <Link to={`/logement/${logements.id}`} className="card-link">
-                <div className="img-container">
-                    <img src={logements.cover} alt={logements.title} />
-                </div>
-                <div className="card-title">
-                    <h2>{logements.title}</h2>
-                </div>
-            </Link>
-        </article>
-    );
+const LogementCard = ({ logement }) => {
+  return (
+    <article className="card">
+      <Link to={`/logement/${logement.id}`} className="card-link">
+        <div className="img-container">
+          <img src={logement.cover} alt={logement.title} />
+        </div>
+        <div className="card-title">
+          <h2>{logement.title}</h2>
+        </div>
+      </Link>
+    </article>
+  );
 };
-
 export default LogementCard;
